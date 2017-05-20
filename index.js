12b01d3da1e8beb4d7b0c165330dd1974f5bf69d
RJ <crongjie@hotmail.com>
2017-05-21 01:29:39 +0900

    added common functions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+    isiOS: function() {
+        return /iPad|iPhone|iPod/.test(navigator.userAgent);
+    },
+    isAndroid: function() {
+        return /(android)/i.test(navigator.userAgent);
+    },
+    isIE: function() {
+        return userAgent.match(/(msie|MSIE)/);
+    }
+};
